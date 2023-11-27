bf77a16f7e812dae8697f0fd8d1b975f19cb5dee
Marcin Stojek <mstojek@gmail.com>
2023-11-27 17:53:52 +0100

    Since the graphs were not visible in Luci on Openwrt I changed default names for plugin name and type name

--- nlbw2collectd.lua
+++ nlbw2collectd.lua
@@ -1,10 +1,10 @@
 require "luci.jsonc"
 require "luci.sys"
 
-local HOSTNAME = '' -- leave empty if you track statistics for local system, change when you really know that you want different hostname to be used
-local PLUGIN="nlbwmon" -- change to 'iptables-mangle' to have full compliance with iptmon
-local PLUGIN_INSTANCE_RX="iptmon_rx"
-local PLUGIN_INSTANCE_TX="iptmon_tx"
+local HOSTNAME = 'testing' -- leave empty if you track statistics for local system, change when you really know that you want different hostname to be used
+local PLUGIN="iptables-mangle"
+local PLUGIN_INSTANCE_RX="nlbwmon_rx" -- change to 'iptmon_rx' to have full compliance with iptmon
+local PLUGIN_INSTANCE_TX="nlbwmon_tx" -- change to 'iptmon_tx' to have full compliance with iptmon
 local TYPE_BYTES="ipt_bytes"
 local TYPE_PACKETS="ipt_packets"
 local TYPE_INSTANCE_PREFIX_RX="rx_"
